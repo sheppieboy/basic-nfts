78940ae30c7b7117e49ce630027067a2c8368daa
Luke Sheppard <sheppiegit@gmail.com>
2023-05-26 15:32:38 -0400

    added the dotenv config and toolbox in the hardhat config file.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,16 @@
 /** @type import('hardhat/config').HardhatUserConfig */
+require('@nomicfoundation/hardhat-toolbox');
+require('dotenv').config();
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 module.exports = {
   solidity: '0.8.18',
+  networks: {
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
