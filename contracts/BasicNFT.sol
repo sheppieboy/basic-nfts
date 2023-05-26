29cf47c439a6a803df9dd0bf4dd610a9226969a0
Luke Sheppard <sheppiegit@gmail.com>
2023-05-26 15:10:26 -0400

    Basic implementation of an NFT using openzeppellin ERC721

---
+++ contracts/BasicNFT.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract BasicNFT is ERC721 {
+    string public constant TOKEN_URI =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+    uint256 private s_tokenCounter;
+
+    constructor() ERC721("Dogie", "DOG") {}
+
+    function mintNFT() public {
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter++;
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {
+        return TOKEN_URI;
+    }
+
+    function getTokenCounter() view publci returns (uint256) {
+        return s_tokenCounter;
+    }
+}
